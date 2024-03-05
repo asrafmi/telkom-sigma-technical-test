a1dc815fdebebb351f463e427c9569fd7fb4e61a
asrafmi <asraf.muhammad07@gmail.com>
2024-03-05 23:07:13 +0700

    feat: init event migration & model

---
+++ backend-ts/src/db/migrations/20240305154133-create-event.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      date: {
+        type: Sequelize.DATE,
+      },
+      location: {
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id',
+          as: 'userId',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Events');
+  },
+};
---
+++ backend-ts/src/db/models/event.model.ts
@@ -0,0 +1,67 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelizeConnection from '../../config/db-connect';
+
+export interface EventAttributes {
+  id?: number;
+  name: string;
+  description: string;
+  date: Date;
+  location: string;
+  user_id: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+class Event extends Model<EventAttributes> implements EventAttributes {
+  public id!: number;
+  public name!: string;
+  public description!: string;
+  public date!: Date;
+  public location!: string;
+  public user_id!: number;
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+
+  public static associate(models: any) {
+    Event.belongsTo(models.User, {
+      foreignKey: 'user_id',
+      onDelete: 'CASCADE',
+    });
+  }
+}
+
+Event.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    location: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: sequelizeConnection,
+    modelName: 'Event',
+  }
+);
+
+export default Event;
