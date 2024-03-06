27e2399b17a97db5692684e15d0db49fd12c1701
asrafmi <asraf.muhammad07@gmail.com>
2024-03-06 14:17:26 +0700

    feat: setup frontend

---
+++ frontend/src/app/navbar.tsx
@@ -0,0 +1,107 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { Disclosure, Menu } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
+
+const navigation = [
+  { name: 'Home', href: '/' },
+  { name: 'Event', href: '/event' },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export default function Navbar({ user }: { user?: any }) {
+  const pathname = usePathname();
+
+  return (
+    <Disclosure as="nav" className="bg-white shadow-sm">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="flex h-16 justify-between">
+              <div className="flex">
+                <div className="flex justify-center items-center">
+                  <Image
+                    className="h-8 w-8 rounded-full"
+                    src="/vercel.svg"
+                    height={32}
+                    width={32}
+                    alt="navbar-logo"
+                  />
+                </div>
+                <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className={classNames(
+                        pathname === item.href
+                          ? 'border-slate-500 text-gray-900'
+                          : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
+                        'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                      )}
+                      aria-current={pathname === item.href ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+              <div className="hidden sm:ml-6 sm:flex sm:items-center">
+                <Menu as="div" className="relative ml-3">
+                  <div>
+                    <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">
+                      <Image
+                        className="h-8 w-8 rounded-full"
+                        src={user?.image || 'https://avatar.vercel.sh/3'}
+                        height={32}
+                        width={32}
+                        alt={`${user?.name || 'placeholder'} avatar`}
+                      />
+                    </Menu.Button>
+                  </div>
+                </Menu>
+              </div>
+              <div className="-mr-2 flex items-center sm:hidden">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <>
+                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    </>
+                  )}
+                </Disclosure.Button>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 pt-2 pb-3">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    pathname === item.href
+                      ? 'bg-slate-50 border-slate-500 text-slate-700'
+                      : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800',
+                    'block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+                  )}
+                  aria-current={pathname === item.href ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
+}
